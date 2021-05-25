52bb3439a356ee55f1486dd21714c5fe320babfd
RopeJuve <rope.s@hotmail.com>
2021-05-25 15:55:14 +0200

    class properties

--- src/App.js
+++ src/App.js
@@ -12,46 +12,39 @@ const Div = styled.div`
 `;
 
 class App extends React.Component{
-
-  constructor(props){
-    super(props)
-    this.state = {
-      showBalance: true,
-      balance: 10000,
-      coinData:[
-        {
-          name:"Bitcoin",
-          tiker: "BTC",
-          balance: 0.5,
-          price: 50000.00
-        },
-        {
-          name:"Ethereum",
-          tiker: "ETH",
-          balance: 32.0,
-          price: 4100.00
-        },
-        {
-          name:"Tether",
-          tiker: "USDT",
-          balance: 0,
-          price: 1.00
-        },
-        {
-          name:"Ripple",
-          tiker: "XPR",
-          balance: 100,
-          price: 0.2
-        }
+  state = {
+    showBalance: true,
+    balance: 10000,
+    coinData:[
+      {
+        name:"Bitcoin",
+        tiker: "BTC",
+        balance: 0.5,
+        price: 50000.00
+      },
+      {
+        name:"Ethereum",
+        tiker: "ETH",
+        balance: 32.0,
+        price: 4100.00
+      },
+      {
+        name:"Tether",
+        tiker: "USDT",
+        balance: 0,
+        price: 1.00
+      },
+      {
+        name:"Ripple",
+        tiker: "XPR",
+        balance: 100,
+        price: 0.2
+      }
 
 
-      ]
-    }
-    this.handleRefresh = this.handleRefresh.bind(this);
-    this.hideBalance = this.hideBalance.bind(this);
+    ]
   }
-
-  handleRefresh(valueChangeTiker){
+  handleRefresh = (valueChangeTiker) =>{
    const newCoinData = this.state.coinData.map(function({name,tiker,price,balance})
    {
      let newPrice = price;
@@ -71,7 +64,7 @@ class App extends React.Component{
 
   }
 
-  hideBalance(){
+  hideBalance = () => {
     this.setState({showBalance:!this.state.showBalance,});
   }
 
